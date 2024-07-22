de6a31825ad753391e30e5bc4ffcbe3bf4ad9b82
ronikrawat <ronikrawat@email>
2024-07-22 23:49:14 +0530

    first commit

---
+++ conftest.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from POM.homepage import Homepage
+from POM.login import LoginPage
+from POM.registration import Registration
+from pytest import fixture
+
+def pytest_addoption(parser):
+    # parser.addoption("--env",action= "store",default="test",dest="env")
+    parser.addoption("--browser", action="store", default="chrome", dest="browser")
+
+
+@fixture
+def driver(request):
+    browser = request.configure.option.browser
+    if browser.upper() == "CHROME":
+        _driver = webdriver.Chrome()
+    elif browser.upper() == "EDGE":
+        _driver = webdriver.Edge()
+    elif browser.upper() == "FIREFOX":
+        _driver = webdriver.Firefox()
+    else:
+        raise Exception("Invalid browser")
+    _driver.get("https://demowebshop.tricentis.com/")
+    _driver.maximize_window()
+    yield _driver
+    _driver.quit()
+
+
+@fixture
+def pages(driver):
+    class Pages:
+        loginpage = LoginPage(driver)
+        homepage = Homepage(driver)
+        registration = Registration(driver)
+
+    return Pages()
